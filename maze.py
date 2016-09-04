d8a9439c27d3730626ee551dd05783c73e258e55
Vikas Jain <vksjn18@gmail.com>
2016-09-05 00:40:43 +0530

    adding comments for understandability

--- maze.py
+++ maze.py
@@ -2,31 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Maze(object):
-	def __init__(self, grid_size, free_cells = [], goal = []):
+	def __init__(self, grid_size, free_states = [], goal = []):
 		self.grid_size = grid_size
-		self.num_actions = 4
-		self.free_cells = free_cells
+		self.num_actions = 4  
+		# four actions in each state -- up, right, bottom, left
+		self.free_states = free_states
 		self.goal = goal
 		self.maze = np.zeros((grid_size,grid_size))
-		for i in self.free_cells:
+		for i in self.free_states:
 			self.maze[i[0]][i[1]] = 1
 
 	def reset(self):
-		self.start_index = np.random.randint(0,len(self.free_cells))
-		self.curr_state = self.free_cells[self.start_index]
+		# reset the environment
+		self.start_index = np.random.randint(0,len(self.free_states))
+		self.curr_state = self.free_states[self.start_index]
 
 	def state(self):
 		return self.curr_state
 
-	def draw(self, path = "", num1 = 1, num2 = 1, num3 = 1):
+	def draw(self, path = "", num1 = 1):
+		# draw the maze configiration
 		self.grid = np.zeros((self.grid_size, self.grid_size))
-		for i in self.free_cells:
+		for i in self.free_states:
 			self.grid[i[1]][i[0]] = 0.5
 		self.grid[self.goal[1]][self.goal[0]] = 1
 		plt.figure(0)
 		plt.clf()
 		plt.imshow(self.grid, interpolation='none', cmap='gray')
-		plt.savefig(path + "%d_%d_%d.png" % (num1, num2, num3))
+		plt.savefig(path + "%d.png" % (num1))
 
 	def act(self, action):
 		if(action == -1):
@@ -40,7 +43,7 @@ class Maze(object):
 		elif(action == 3):
 			self.next_state = [self.curr_state[0],self.curr_state[1]-1]
 
-		if ((self.next_state in self.free_cells) or (self.next_state == self.goal)):
+		if ((self.next_state in self.free_states) or (self.next_state == self.goal)):
 			self.curr_state = self.next_state
 			self.reward = 0
 		else:
